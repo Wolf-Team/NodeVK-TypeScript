24ea6539e1e40a55c850a7ac63db2198b42a1c84
80LK <80LK@users.noreply.github.com>
2021-10-26 11:56:09 +0300

    remove token
    Token was been removed in VK

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Session from "./Session.js";
 (async () => {
 	const session = new Session({
-		token: "734ff6b5e31db7e973e44d563c0a6102a9840888680610a9b3acfd684b29ffc1ce7cd887048d4844e9680"
+		token: ""
 	});
 	try {
 		console.log(await session.groups.getMembers(200036647, 0, 1000));
